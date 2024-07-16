59c4e12ba725a655004fbc5c716750aa9c40e449
Legun George <georgelegun@gmail.com>
2024-07-16 13:38:44 +0300

    Initial commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,112 @@
+from flask import Flask, session, redirect, url_for, render_template, request
+import typing
+import sqlite3
+import asyncio
+import threading
+import base64
+from . import ws
+from . import defs
+
+
+class FlaskApplication:
+   flask_app: Flask
+   db_connection: sqlite3.Connection
+   available_image_count: int # 0 - newest, available_image_count - 1 oldest
+   def __init__(self, db_schema_path: str, db_path: str):
+      # DB
+      self.db_connection = sqlite3.connect(db_path, check_same_thread=False)
+      print(f"DB: Connected {db_path!r}")
+      with open(db_schema_path, "r", encoding="utf-8") as f:
+         self.db_connection.cursor().executescript(f.read())
+      self.available_image_count = int(self.db_connection.cursor().execute("""SELECT count() FROM history""").fetchone()[0])
+      self.db_connection.commit()
+      print(f"DB: Initialized")
+      
+      # Flask
+      self.flask_app = Flask(__name__)
+      self.flask_app.config.from_mapping(
+         SECRET_KEY="dev"
+      )
+      print(f"FLASK: App created")
+      
+   def serve_ws_select(self, request) -> typing.Tuple[str, int]: 
+      """
+      request: {item: int} | null
+      """
+      if request == None:
+         print(f"REQ IMG: LATEST")
+  
+      rid = int(request.item if request != None else 0)
+      print(f"REQ IMG: {rid}")
+      
+      if rid >= self.available_image_count:
+         print(f"REQ: No images present")
+         return ("", 0)
+      
+      # FETCH image number {request.item} from db
+      cursor = self.db_connection.cursor()
+      img_query = cursor.execute("""
+                                 SELECT img FROM history
+                                 ORDER BY id DESC
+                                 LIMIT 1 OFFSET ?;
+                                 """, (rid,) ).fetchone()[0]
+      if type(img_query) != bytes:
+         raise Exception(f"Invalid db response. Expected bytes but got {type(img_query)}")
+      return (base64.b64encode(img_query).decode("ascii"), rid)
+   
+def create_app(test_config=None) -> Flask: 
+   appd = FlaskApplication(db_schema_path="./historydb.schema.sql", db_path="./historydb.sqlite")
+   
+   threading.Thread(target=asyncio.run, args=(ws.run_server(appd),) ).start()
+   threading.Thread(target=asyncio.run, args=(ws.handle_new_image_event(appd),) ).start()
+   # asyncio.get_event_loop().run_forever(ws.run_server()) 
+   # print(f"WS: Running")
+
+   @appd.flask_app.route("/")
+   def f_root():
+      return redirect(url_for("f_index_1"))
+   
+   @appd.flask_app.route("/index")
+   def f_index_1():
+      print(f"FLASK: /index/0")
+      return render_template("index.html", available_image_count=appd.available_image_count, image_to_request=0)
+   
+   @appd.flask_app.route("/index/<int:img_to_request>")
+   def f_index_2(img_to_request):
+      print(f"FLASK: /index/{img_to_request}")
+      return render_template("index.html", available_image_count=appd.available_image_count, image_to_request=img_to_request)
+   
+   
+   @appd.flask_app.route("/ingest/new-image", methods=["POST"])
+   def f_ingest_image():
+      if type(request.data) is bytes:
+         print(f"FLASK: /ingest/new-image Recv {len(request.data)} bytes")
+         # DB: Add image
+         cursor = appd.db_connection.cursor()
+         cursor.execute(""" INSERT INTO history (img) VALUES (?) """, (request.data,))
+         # DB: Cap at 10 images max
+         img_count = cursor.execute(""" SELECT count() FROM history""").fetchone()[0]
+         if type(img_count) != int: 
+            raise TypeError(f"Expected int, but got {type(img_count)}")
+         if img_count > defs.MAX_DB_IMAGE_COUNT:
+            print(f"DB: Image count: {img_count} - {defs.MAX_DB_IMAGE_COUNT} => rem {img_count - defs.MAX_DB_IMAGE_COUNT}")
+            cursor.execute(""" 
+                           DELETE FROM history
+                           WHERE id IN (
+                              SELECT id FROM history
+                              ORDER BY id
+                              LIMIT ?
+                           );
+                           """, (img_count - defs.MAX_DB_IMAGE_COUNT,))
+            img_count = defs.MAX_DB_IMAGE_COUNT
+         
+         appd.db_connection.commit()
+         print(f"DB: added image - new count: {img_count}")
+         appd.available_image_count = img_count
+         defs.NEW_IMG_EVENT.set()
+         return "OK"
+      else:
+         print(f"FLASK: /ingest/new-image invalid data: {type(request.data)}")
+         return "INVALID DATA"
+   
+   return appd.flask_app
---
+++ flaskr/ws.py
@@ -0,0 +1,74 @@
+import asyncio
+import websockets
+import time
+import sqlite3
+import base64
+import json
+import threading
+from types import SimpleNamespace
+from . import defs
+
+# WS SERVER
+CONN_WS_CLIENTS = set()
+
+async def handle_new_image_event(flas_appd):
+   while True:
+      print(f"NEW_IMG_EVENT: Waiting")
+      defs.NEW_IMG_EVENT.wait()
+      print(f"NEW_IMG_EVENT: Triggered")
+      
+      for c in CONN_WS_CLIENTS.copy():
+         print(f"WS BROADCAST: SEND EV SELECT_RESP")
+         try:
+            await c.send(json.dumps({"event": "UPDATE"}))
+         except Exception as e:
+            print(f"Error broadcasting to client: {e}; Removing")
+            CONN_WS_CLIENTS.remove(c)
+      
+      defs.NEW_IMG_EVENT.clear()
+   
+async def run_server(flask_appd):
+   async def handler(websocket, path):
+      print(f"Client connected: {websocket.remote_address} - {path}")
+      CONN_WS_CLIENTS.add(websocket)
+      try:
+         async for message in websocket:
+            try: 
+               m = json.loads(message, object_hook=lambda d: SimpleNamespace(**d))
+               print(f"WS: EV: {m.event}")
+               
+               if m.event == "SELECT":
+                  # Request body: {
+                  #    event: "SELECT",
+                  #    data: { item: int } | null
+                  # }
+                  d = None
+                  try:
+                     d = m.data
+                  except:
+                     d = None
+                     
+                  (imgBase64, currentSelection) = flask_appd.serve_ws_select(d)
+                  print(f"WS: SEND EV SELECT_RESP")
+                  await websocket.send(json.dumps({
+                     "event": "SELECT_RESP", 
+                     "data": {
+                        "imgBase64": imgBase64,
+                        "currentSelection": currentSelection
+                     }}))
+                  
+            except Exception as ex:
+               print(f"WS: ERR: {ex}")
+      except websockets.ConnectionClosed:
+         print(f"Client disconnected: {websocket.remote_address}")
+         CONN_WS_CLIENTS.remove(websocket)
+              
+   while True:
+      try:
+         async with websockets.serve(handler, "localhost", 4999):
+            print("WebSocket server started on ws://localhost:4999")
+            await asyncio.Future()  # Run forever
+         return
+      except Exception as ex:
+         print(f"Error running ws server: {ex}")
+         time.sleep(4)
