d067b0b7f7916329f122bc5d69aa6c469f4a3378
George Legun <georgelegun@gmail.com>
2024-08-07 15:22:29 +0300

    host

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-podman build . -t flaskshowimg --build-arg VERSION=1.0.8 --build-arg HOST="devbuild-oel.kbclab.ro" \
+podman build . -t flaskshowimg --build-arg VERSION=1.0.8 --build-arg HOST="..." \
    && podman-compose up -d --force-recreate
